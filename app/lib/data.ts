a509e07cf5f18c05f74d74f7da92fdc93378c0a3
Ahmed Hassan Mohi <a7med.hassan.mohi@gmail.com>
2023-11-08 21:25:44 +0200

    chapter 13 handling error

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -159,7 +159,6 @@ export async function fetchInvoiceById(id: string) {
       FROM invoices
       WHERE invoices.id = ${id};
     `;
-
     const invoice = data.rows.map((invoice) => ({
       ...invoice,
       // Convert amount from cents to dollars
